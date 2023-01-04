d8ad8a618b298169fe41e15e53326c970b9554d4
Inseok Lee <git@dlun.ch>
2023-01-04 18:22:37 +0900

    Use wsl ssh agent, vscode

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,13 @@
 set -x KUBECONFIG ~/.kube/config:$KUBECONFIG
 
-set -x GPG_TTY (tty)
-gpgconf --launch gpg-agent
+set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+set ALREADY_RUNNING (ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $status)
+
+if [ $ALREADY_RUNNING != "0" ]
+	rm -f $SSH_AUTH_SOCKET
+    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/d/Programs/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
+	disown
+end
+
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
