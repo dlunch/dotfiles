874ed4fa3892d3086f388ee24b87a508529f68ef
Inseok Lee <git@dlun.ch>
2024-02-12 10:42:01 +0900

    New wsl environment

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,7 @@
 set -x KUBECONFIG ~/.kube/config:$KUBECONFIG
 
-set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
-set ALREADY_RUNNING (ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $status)
-
-if [ $ALREADY_RUNNING != "0" ]
-	rm -f $SSH_AUTH_SOCKET
-    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/d/Programs/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
-	disown
-end
+alias ssh="ssh.exe"
+alias ssh-add="ssh-add.exe"
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
